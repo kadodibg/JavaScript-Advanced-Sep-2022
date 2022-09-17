e44018852caad330543b54d67df656986a806f5f
kadodibg <kadodi@gmail.com>
2022-09-17 06:11:18 +0300

    New&Up

---
+++ SyntaxFunctionsAndStatements/08.daysInAMonth.js
@@ -0,0 +1,12 @@
+// Month in JavaScript is 0-indexed (January is 0, February is 1, etc), 
+// but by using 0 as the day it will give us the last day of the prior
+// month. So passing in 1 as the month number will return the last day
+// of January, not February
+function daysInAMonth(month, year) {
+    // Here January is 1 based
+    //Day 0 is the last day in the previous month
+    return new Date(year, month, 0).getDate();
+    // Here January is 0 based
+    // return new Date(year, month+1, 0).getDate();
+}
+console.log(daysInAMonth(1, 2012));
